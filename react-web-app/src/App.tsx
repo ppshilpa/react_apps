d71893f2b63ea152ce77c0c27d38f7fb77ead426
ppshilpa <shilpappawar1986@gmail.com>
2022-11-24 10:54:26 +0530

    added product list

--- react-web-app/src/App.tsx
+++ react-web-app/src/App.tsx
@@ -3,27 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import ProductList from "./component/ProductList";
 import products from "./data/products.json";
-interface product {
-  availableSizes: string[];
-  currencyFormat: string;
-  currencyId: string;
-  description: string;
-  id: number;
-  installments: number;
-  isFreeShipping: boolean;
-  price: number;
-  sku: number;
-  style: string;
-  title: string
-};
 
 const App =() =>{
   return (
     <div  className="App container-fluid">
-      <header className="App-header">
-       
-      </header>
-      <ProductList data={products as product[]} />
+      <ProductList data={products} cardName={"test name"} />
     </div>
   );
 }
--- react-web-app/src/component/ProductList.tsx
+++ react-web-app/src/component/ProductList.tsx
@@ -1,59 +1,36 @@
-interface product {
+import {ProductCard} from './ProductCard';
+
+export interface product {
     availableSizes: string[];
     currencyFormat: string;
     currencyId: string;
     description: string;
     id: number;
-    installments: number;
+    installments?: number;
     isFreeShipping: boolean;
-    price: number;
+    price: number | string;
     sku: number;
     style: string;
     title: string
   };
-  type listOfProps = {
-    data:product[];
-    value?:string;
+ 
+type propsList ={
+data :product[];
+cardName?: string;
+};
 
-}
-const ProductCard = (props:product) => {
-    return (
-      <div className="product">
-        <p>
-          <b>Title:</b> {props.title}
-        </p>
-        <p>
-          <b>Style:</b> {props.style}
-        </p>
-        <p>
-          <b>Price:</b> {props.price}
-        </p>
-        <p>
-          <b>Description:</b> {props.description}
-        </p>
-        <p>
-          <b>Free shipping:</b> {props.isFreeShipping}
-        </p>
-        <hr />
-      </div>
-    );
-  };
-   const ProductsList = (props:listOfProps) => {
-    console.log(props);
-    let { data: products } = props;
+   const ProductsList = (props:propsList) => {
+   // const products = props.data || []
+    const {data:productList, cardName} = props;
     return (
       <div>
         <div>
           <div>
-            <h2>Products</h2>
+            <h2>{cardName}</h2>
           </div>
         </div>
-         <div>
-          {products.map((product) => {
-            console.log(product);
-         return <ProductCard key={product.sku} {...product} />
-          }
-          )}
+         <div className="row">
+          {productList.map((product) => <div className='col-3'><ProductCard key={product.sku} {...product} /></div>)}
         </div> 
       </div>
     )
