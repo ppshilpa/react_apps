fd8673d3fddca9962be84011a65fe8472f4354b1
ppshilpa <shilpappawar1986@gmail.com>
2022-11-25 15:46:27 +0530

    add to cart

--- react-web-app/src/component/CartModel.tsx
+++ react-web-app/src/component/CartModel.tsx
@@ -5,42 +5,43 @@ export const CartModel = () => {
             <div className="modal-dialog">
                 <div className="modal-content">
                     <div className="modal-header">
-                        <h5 className="modal-title">Modal title</h5>
+                        <h5 className="modal-title">Add to Cart</h5>
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body">
-                        <form className="row g-3 needs-validation" >
-                            <div className="col">
-                                <select className="form-select" >
-                                    <option >Select Quantity</option>
-                                    <option value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
-                                    <option value="4">Four</option>
-                                    <option value="5">Five</option>
-                                </select>
+                    <form className="row g-3 needs-validation" >
+                        <div className="form-group">
+                            <select className="form-select" >
+                                <option >Select Quantity</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                                <option value="4">Four</option>
+                                <option value="5">Five</option>
+                            </select>
+                        </div>
+                        <div className="form-group">
+                            <label>Select Size</label><br />
+                            <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="radio" name="size" value="option1" />
+                                <label className="form-check-label" >S</label>
                             </div>
-                            <div>
-                                <div className="form-check form-check-inline">
-                                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
-                                    <label className="form-check-label" >S</label>
-                                </div>
-                                <div className="form-check form-check-inline">
-                                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
-                                    <label className="form-check-label" >M</label>
-                                </div>
-                                <div className="form-check form-check-inline">
-                                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" />
-                                    <label className="form-check-label" >XL </label>
-                                </div>
+                            <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="radio" name="size" value="option2" />
+                                <label className="form-check-label" >M</label>
                             </div>
-                        </form>
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" className="btn btn-primary">Save changes</button>
-                    </div>
+                            <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="radio" name="size" value="option3" />
+                                <label className="form-check-label" >XL </label>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" className="btn btn-primary">Checkout</button>
                 </div>
             </div>
-        </div>)
+        </div>
+        </div >)
 }
